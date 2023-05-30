6a998515db694e883a820e20d62e3e0c67e397ff
colecperry <colecperry@gmail.com>
2023-05-30 14:54:15 -0700

    Fixed navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -19,38 +19,80 @@ function NavBar () {
 
 
     return (
-        <Navbar bg="primary" variant="dark" expand="lg" sticky="top" style={{margin: "10px"}}>
-        {/* <img src="https://i.postimg.cc/k5q6BfbH/4096325394.gif" alt="logo" style={{marginLeft: "20px", width:"30px", height:"30px"}}/> */}
+        // <Navbar bg="primary" variant="dark" expand="lg" sticky="top" style={{margin: "10px"}}>
+        // {/* <img src="https://i.postimg.cc/k5q6BfbH/4096325394.gif" alt="logo" style={{marginLeft: "20px", width:"30px", height:"30px"}}/> */}
     
-            <Navbar.Brand style={{margin: "10px"}}>
-                Doggio
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            < Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link as={NavLink} to="/home">
-                        <FaDog /> Home
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to="/profile">
-                        <FaUser /> Profile
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to="/messages">
-                        <FaEnvelope /> Messages
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to="/favorites">
-                        <FaHeart /> Favorites
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to="/checkout">
-                        <FaCartPlus /> Checkout
-                    </Nav.Link>
-                    {/* <NavDropdown title={<><FaBell /> Notifications</>} id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#notification"><FaBell /> Notification</NavDropdown.Item>
-                        <NavDropdown.Item href="#finances"><FaDollarSign /> Finances</NavDropdown.Item>
-                    </NavDropdown> */}
-                    <button onClick={() => handleLogoutClick()}>Logout</button>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        //     <Navbar.Brand style={{margin: "10px"}}>
+        //         Doggio
+        //     </Navbar.Brand>
+        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //     < Navbar.Collapse id="basic-navbar-nav">
+        //         <Nav className="mr-auto">
+        //             <Nav.Link as={NavLink} to="/home">
+        //                 <FaDog /> Home
+        //             </Nav.Link>
+        //             <Nav.Link as={NavLink} to="/profile">
+        //                 <FaUser /> Profile
+        //             </Nav.Link>
+        //             <Nav.Link as={NavLink} to="/messages">
+        //                 <FaEnvelope /> Messages
+        //             </Nav.Link>
+        //             <Nav.Link as={NavLink} to="/favorites">
+        //                 <FaHeart /> Favorites
+        //             </Nav.Link>
+        //             <Nav.Link as={NavLink} to="/checkout">
+        //                 <FaCartPlus /> Checkout
+        //             </Nav.Link>
+        //             {/* <NavDropdown title={<><FaBell /> Notifications</>} id="basic-nav-dropdown">
+        //                 <NavDropdown.Item href="#notification"><FaBell /> Notification</NavDropdown.Item>
+        //                 <NavDropdown.Item href="#finances"><FaDollarSign /> Finances</NavDropdown.Item>
+        //             </NavDropdown> */}
+        //             <button onClick={() => handleLogoutClick()}>Logout</button>
+        //         </Nav>
+        //     </Navbar.Collapse>
+        // </Navbar>
+        <div>
+            <h1 style={{
+            paddingTop:"5px",
+            paddingBottom:"5px",
+            fontSize:"3.5rem",
+            textAlign: "center",
+            fontFamily: 'Verdana, sans-serif'
+            }}>Doggio
+            <FaDog/>
+            </h1> 
+            <div class="ui secondary pointing menu"
+            style={{
+                marginBottom: "10px",
+                fontSize: "1.4rem"
+            }}>
+            <a class="active item">
+                <Nav.Link as={NavLink} to="/home">
+                    <FaDog /> Home
+                </Nav.Link>
+            </a>
+            <a class="item">
+                <Nav.Link as={NavLink} to="/profile">
+                    <FaUser /> Profile
+                </Nav.Link>
+            </a>
+            <a class="item">
+                <Nav.Link as={NavLink} to="/messages">
+                    <FaEnvelope /> Messages
+                </Nav.Link>
+            </a>
+                    <a class="item">
+                <Nav.Link as={NavLink} to="/favorites">
+                    <FaHeart /> Favorites
+                </Nav.Link>
+            </a>
+                <div class="right menu">
+                <a class="ui item" onClick={() => handleLogoutClick()}>
+                    <b>Logout</b>
+            </a>
+            </div>
+            </div>
+        </div>
     )
 }
 
