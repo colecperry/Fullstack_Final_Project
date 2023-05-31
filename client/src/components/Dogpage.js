bec876f67f26314eab86fb0cffd6b81647903b21
colecperry <colecperry@gmail.com>
2023-05-31 11:47:35 -0700

    Finished styling dogpage

--- client/src/components/Dogpage.js
+++ client/src/components/Dogpage.js
@@ -88,13 +88,11 @@ function DogPage() {
                 </div>
                 {dog && (
                     <div className="dog-details" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                    <div style={{ display: "flex", alignItems: "center", marginTop: "10px", marginBottom: "10px"}}>
+                    <div style={{ display: "flex", alignItems: "center", marginTop: "10px", marginBottom: "0px"}}>
                         <h1 style={{ marginRight: "10px"}}>{dog.dog_name}</h1>
-                        <Icon className='likeIcon' color={(specificLike) ? "green" : "grey"} name='heart' onClick={handleLikeButton} style={{fontSize: "22px", display: "flex", alignItems: "center", marginBottom: "10px" }}/>
-                        {/* {isLiked ?
-                        // <FaHeart classname="fullHeart" onClick={() => window.alert("You've already liked this post!")}/>
-                        // : <FaRegHeart classname="emptyHeart" onClick={onLikeButtonClick} />} */}
+                        <Icon className='likeIcon' color={(specificLike) ? "red" : "grey"} name='heart' onClick={handleLikeButton} style={{fontSize: "22px", display: "flex", alignItems: "center", marginBottom: "10px" }}/>
                     </div>
+                    <div>
                     <button className="ui primary button" onClick={() => {
                         setBreeder(dog.user)
                         navigate("/messages")
@@ -102,6 +100,7 @@ function DogPage() {
                     }>
                         Contact {firstName}
                     </button>
+                    </div>
                         <div style={{
                         display: "flex",
                         flexDirection: "row",
@@ -109,91 +108,150 @@ function DogPage() {
                         marginTop: "15px"
 
                     }}>
-                        <Container style={{
-                            width: "650px",
-                            marginRight: "10px",
-                            fontSize: "18px"
-                        }}>
-                            <h2 style={{ marginLeft: "0" }}>About {dog.dog_name}</h2>
-                            <p>
-                                {dog.dog_description}
-                            </p>
-                        </Container>
-                        <div style={{ width: "15px" }}></div>
-                        <Container style={{
-                            border: "1px solid black",
-                            padding: "10px",
-                            width: "300px",
-                            display: "flex",
-                            flexDirection: "row",
-                            flexWrap: "wrap",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            marginLeft: "10px"
-                        }}>
-                            <h2>Puppy Snapshot</h2>
-                            <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+                        <div style={{ display: "flex", flexDirection: "column" }}>
+                            <Container style={{
+                                width: "900px",
+                                marginRight: "10px",
+                                fontSize: "18px",
+                                display: "flex",
+                                flexDirection: "column",
+                                marginBottom: "20px",
+                                marginTop: "10px",
+                                
+
+                            }}>
+                                <h2 style={{ marginLeft: "0" }}>About {dog.dog_name}</h2>
                                 <p>
-                                    <strong>Breed:</strong> {dog.dog_breed}
+                                    {dog.dog_description}
                                 </p>
-                                <p>
-                                    <strong>Gender:</strong> {dog.dog_gender}
-                                </p>
-                                <p>
-                                    <strong>Age:</strong> {dog.dog_age}
-                                </p>
-                                <p>
-                                    <strong>Weight:</strong> {dog.dog_weight}
-                                </p>
-                                <p>
-                                    <strong>Color:</strong> {dog.dog_color}
-                                </p>
-                                <p>
-                                    <strong>Price:</strong> ${dog.dog_price}
-                                </p>
-                            </div>
-                        </Container>
-                    </div>
-                    <Container style={{
-                            border: "1px solid black",
-                            padding: "10px",
-                            width: "550px",
-                            display: "flex",
-                            flexDirection: "row",
-                            flexWrap: "wrap",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            marginTop: "15px",
-                            marginBottom: "10px"
-                        }}>
-                            <h2>Parent Spotlight</h2>
-                            <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridRowGap: "10px", gridColumnGap: "15px" }}>
-                                <p>
-                                    <strong>Mother's Name:</strong> {dog.mother_name}
-                                </p>
-                                <p>
-                                    <strong>Father's Name:</strong> {dog.father_name}
-                                </p>
-                                <p>
-                                    <strong>Mother's Breed:</strong> {dog.mother_breed}
-                                </p>
-                                <p>
-                                    <strong>Father's Breed:</strong> {dog.father_breed}
-                                </p>
-                                <p>
-                                    <strong>Mother's Weight:</strong> {dog.mother_weight}
-                                </p>
-                                <p>
-                                    <strong>Father's Weight:</strong> {dog.father_weight}
-                                </p>
-                                <p>
-                                    <strong>Mother's Age:</strong> {dog.mother_age}
-                                </p>
-                                <p>
-                                    <strong>Father's Age:</strong> {dog.father_age}
-                                </p>
-                                </div>
                             </Container>
+                            <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
+                                <table className="ui celled striped table" style={{
+                                    padding: "10px",
+                                    width: "300px",
+                                    marginLeft: "20px",
+                                    textAlign: "center",
+                                }}>
+                                    <thead>
+                                        <tr><th colspan="3">
+                                        Puppy Snapshot
+                                        </th>
+                                    </tr></thead>
+                                    <tbody>
+                                        <tr>
+                                        <td class="collapsing">
+                                            Breed
+                                        </td>
+                                        <td>{dog.dog_breed}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Gender
+                                        </td>
+                                        <td>{dog.dog_gender}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Age
+                                        </td>
+                                        <td>{dog.dog_age}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Weight
+                                        </td>
+                                        <td>{dog.dog_weight}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Price
+                                        </td>
+                                        <td>${dog.dog_price}</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                    <table className="ui celled striped table" style={{
+                                    padding: "10px",
+                                    width: "300px",
+                                    marginLeft: "20px",
+                                    textAlign: "center",
+                                    alignSelf: "flex-start",
+                                    // marginBottom: "10px"
+                                }}>
+                                    <thead>
+                                        <tr><th colspan="3">
+                                        Mother
+                                        </th>
+                                    </tr></thead>
+                                    <tbody>
+                                        <tr>
+                                        <td class="collapsing">
+                                            Name
+                                        </td>
+                                        <td>{dog.mother_name}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Breed
+                                        </td>
+                                        <td>{dog.mother_breed}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Weight
+                                        </td>
+                                        <td>{dog.mother_weight}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Age
+                                        </td>
+                                        <td>{dog.mother_age}</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                    <table className="ui celled striped table" style={{
+                                    padding: "10px",
+                                    width: "300px",
+                                    marginLeft: "20px",
+                                    textAlign: "center",
+                                    alignSelf: "flex-start",
+                                }}>
+                                    <thead>
+                                        <tr><th colspan="3">
+                                        Father
+                                        </th>
+                                    </tr></thead>
+                                    <tbody>
+                                        <tr>
+                                        <td class="collapsing">
+                                            Name
+                                        </td>
+                                        <td>{dog.father_name}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Breed
+                                        </td>
+                                        <td>{dog.father_breed}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Weight
+                                        </td>
+                                        <td>{dog.father_weight}</td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            Age
+                                        </td>
+                                        <td>{dog.father_age}</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                </div>
+                                </div>
+                            </div>
                     </div>
                 )}
                 </div>
@@ -201,3 +259,4 @@ function DogPage() {
         }
 
 export default DogPage;
+
